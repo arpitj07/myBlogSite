e5d67eb40372bacd5cf7773c6dd73979958c9fa6
arpitj07 <arpitjain070295@gmail.com>
2022-01-25 03:23:33 +0530

    Register Component Created

--- src/components/Register/Register.styles.js
+++ src/components/Register/Register.styles.js
@@ -4,6 +4,8 @@ export const Wrapper = styled.div`
 	width: 100%;
 	height: 100%;
 	display: flex;
+
+	margin-top: 120px;
 	align-items: center;
 	justify-content: center;
 	flex-direction: column;
@@ -11,17 +13,31 @@ export const Wrapper = styled.div`
 
 export const Container = styled.div`
 	width: 500px;
-	height: 100%;
+	height: 400px;
 	display: flex;
 	padding: 30px 20px;
 	align-items: center;
 	justify-content: center;
 	flex-direction: column;
-	border: 1px solid red;
-	border-radius: 20px;
+	box-shadow: 10px 5px 20px;
 
-	label {
-		padding: 10px 0;
+	h3 {
+		margin-bottom: 20px;
+		font-size: 1.7rem;
 		font-weight: 700;
+	}
+	input {
+		margin-top: 20px;
+		width: 350px;
+		height: 40px;
+	}
+
+	button {
+		background-color: black;
+		color: white;
+		width: 190px;
+		height: 40px;
+		margin-top: 20px;
+		border-radius: 20px;
 	}
 `;
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,21 +1,98 @@
 import React from 'react';
+
 import { Wrapper, Container } from './Register.styles';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+//Firebase Dependensies
+import { collection, addDoc } from 'firebase/firestore';
+import { db, auth } from '../../firebase-config';
+
 export const Register = () => {
+	const [ registerEmail, setRegisterEmail ] = useState('');
+	const [ registerPassword, setRegisterPassword ] = useState('');
+	const [ phoneNum, setPhoneNum ] = useState('');
+	const [ FullName, setFullName ] = useState('');
+
+	const navigate = useNavigate();
+	const postCollection = collection(db, 'UserData');
+
+	const regDetails = async () => {
+		try {
+			const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+			await addDoc(postCollection, { username: FullName, email: registerEmail });
+			console.log(user);
+			navigate('/login');
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<React.Fragment>
 			<Wrapper>
-				<h1>Welcome to myBlog</h1>
 				<Container>
-					<label htmlFor="Fullname">Full Name:</label>
-					<input type="text" name="Fullname" id="Fullname" />
-					<label htmlFor="PhoneNum">Phone Number:</label>
-					<input type="tel" name="PhoneNum" id="PhoneNum" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
-					<label htmlFor="EmailID">Email:</label>
-					<input type="email" name="EmailID" id="EmailID" />
-					<label htmlFor="Password">Password:</label>
-					<input type="Password" name="Password" id="Password" />
-					<label htmlFor="Password">Confirm Password:</label>
-					<input type="Password" name="Password" id="Password" />
+					<h3>Register User</h3>
+
+					<input
+						type="text"
+						name="Fullname"
+						placeholder="Fullname"
+						id="Fullname"
+						onChange={(e) => {
+							setFullName(e.target.value);
+						}}
+					/>
+
+					<input
+						type="tel"
+						name="PhoneNum"
+						id="PhoneNum"
+						placeholder="phonenumber"
+						pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+						onChange={(e) => {
+							setPhoneNum(e.target.value);
+						}}
+					/>
+
+					<input
+						type="text"
+						name="EmailID"
+						placeholder="email"
+						id="EmailID"
+						onChange={(e) => {
+							setRegisterEmail(e.target.value);
+						}}
+					/>
+
+					<input
+						type="Password"
+						name="Password"
+						placeholder="password"
+						id="Password"
+						onChange={(e) => {
+							setRegisterPassword(e.target.value);
+						}}
+					/>
+
+					<input
+						type="Password"
+						name="Cassword"
+						placeholder="confirm password"
+						id="CPassword"
+						onChange={(e) => {
+							setRegisterPassword(e.target.value);
+						}}
+					/>
+
+					<button
+						onClick={() => {
+							regDetails();
+						}}
+					>
+						Register
+					</button>
 				</Container>
 			</Wrapper>
 		</React.Fragment>
