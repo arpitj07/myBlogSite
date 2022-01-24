e23327f5b0bb948421b23b9d967c436bdf166657
arpitj07 <arpitjain070295@gmail.com>
2022-01-25 03:27:53 +0530

    Coded added for Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// Components
+import { Navbar } from './components/NavBar';
+import { Blogs } from './components/Blogs';
+import { HomePage } from './components/HomePage';
 import { Register } from './components/Register';
+import { Login } from './components/Login';
+import { Posts } from './components/Posts';
+// Router
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<Register />
-		</div>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/createposts" element={<Blogs />} />
+				{/* <Route path="/posts" element={<Posts />} /> */}
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
